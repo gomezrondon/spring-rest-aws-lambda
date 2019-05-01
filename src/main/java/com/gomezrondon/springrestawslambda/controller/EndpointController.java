cd18de1e1f7226abc83ac18d724c2cf16bc2bb12
cajgomez <Jgomez@cambridgeassociates.com>
2019-05-01 10:42:34 -0600

    Adding 2nd endpoint /v2/employee

--- src/main/java/com/gomezrondon/springrestawslambda/controller/EndpointController.java
+++ src/main/java/com/gomezrondon/springrestawslambda/controller/EndpointController.java
@@ -4,6 +4,7 @@ import com.gomezrondon.springrestawslambda.entitie.Employee;
 import com.gomezrondon.springrestawslambda.service.EmployeeService;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -21,5 +22,22 @@ public class EndpointController {
         List<Employee> list = service.findByLasName(name);
           return list;
     }
+
+    @PutMapping("/employee")
+    public Employee saveEmployee(@RequestBody Employee employee){
+        Employee emp = employee;
+        System.out.println("employee :"+emp.toString()+" Was created");
+        return emp;
+    }
+
+    @PutMapping("/v2/employee")
+    public Employee saveEmployeeV2(@RequestBody Employee employee){
+        String time = LocalDateTime.now().toString();
+        employee.setLocation(time);
+        Employee emp = employee;
+        System.out.println("employee :"+emp.toString()+" Was created At:"+time);
+        return emp;
+    }
+
 
 }
