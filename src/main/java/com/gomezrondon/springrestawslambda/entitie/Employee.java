39aaad7a6ded303b6c23f73ab97409279bc03ece
cajgomez <Jgomez@cambridgeassociates.com>
2020-03-08 07:44:03 -0600

    Fixing table name for gcp mysql.

--- src/main/java/com/gomezrondon/springrestawslambda/entitie/Employee.java
+++ src/main/java/com/gomezrondon/springrestawslambda/entitie/Employee.java
@@ -7,10 +7,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Data
 @NoArgsConstructor
+@Table(name = "employee")
 public class Employee {
 
     @Id
