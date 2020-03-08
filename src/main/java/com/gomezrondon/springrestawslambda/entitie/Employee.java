6a146ee848329ef0783106f25c45a5c9658a7f04
cajgomez <Jgomez@cambridgeassociates.com>
2020-03-07 23:12:44 -0600

    Adding lombok and updating to spring 2.2.5

--- src/main/java/com/gomezrondon/springrestawslambda/entitie/Employee.java
+++ src/main/java/com/gomezrondon/springrestawslambda/entitie/Employee.java
@@ -1,86 +1,36 @@
 package com.gomezrondon.springrestawslambda.entitie;
 
 
-import javax.persistence.Column;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class Employee {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-  //  @Column(name = "firstsddname")
-    private String firstName;
-
-   // @Column(name = "lastname")
-    private String lastName;
-
+    private String  firstName;
+    private String  lastName;
     private String primary_skills;
     private String location; // 3 char(3)
 
-    public Employee() {
-    }
-
-    public Employee(Long id, String firstName, String lastName) {
-        this.id = id;
+    public Employee(String  firstName, String  lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
-    public Employee(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPrimary_skills() {
-        return primary_skills;
-    }
-
-    public void setPrimary_skills(String primary_skills) {
-        this.primary_skills = primary_skills;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", primary_skills='" + primary_skills + '\'' +
-                ", location='" + location + '\'' +
-                '}';
-    }
 }
+
+/*@Getter
+@Setter
+@NoArgsConstructor
+@ToString(exclude = {"location"})
+@EqualsAndHashCode*/
